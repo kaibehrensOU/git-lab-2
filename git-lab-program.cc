3cfe42b4299fa6910822e3217ea2a4a16cde5f08
Kai Behrens <kb252523@ohio.edu>
2024-09-04 17:29:44 -0400

    Created program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Kai Behrens>
+ *        Date: <4 Sep 2024>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
